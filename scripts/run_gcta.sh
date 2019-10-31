abf3e77b547cddd5510127eaf2b5be4c691373f9
Murray Cadzow <murray.cadzow@otago.ac.nz>
2019-10-31 17:52:49 +1300

    trying to get gcta to work

--- scripts/run_gcta.sh
+++ scripts/run_gcta.sh
@@ -15,14 +15,14 @@ DIR=results/${POP}_${TRAIT}
 
 #reml
 # $(seq 1 to how many trait/residual columns 
-#software/gcta64 --reml --grm results/${POP}_gcta_grm --pheno tmp/${BFILE}.fam --mpheno $MODEL --prevalence 0.15 --out $DIR/GCTA/${TRAIT}_${CV}_${MODEL}
+software/gcta64 --reml --grm results/${POP}_gcta_grm --pheno tmp/${BFILE}.fam --mpheno $MODEL --prevalence 0.15 --out $DIR/GCTA/${TRAIT}_${CV}_${MODEL}
 software/gcta64 --reml-pred-rand --grm results/${POP}_gcta_grm --pheno tmp/${BFILE}.fam --mpheno ${MODEL} --out ${DIR}/GCTA/${TRAIT}_${CV}_${MODEL}
 
 ### One GRM (case-control studies)
 ### parallel "software/gcta64 --reml-pred-rand --grm $DIR/GCTA/grm --pheno tmp/traindata.fam --mpheno {} --prevalence 0.1 --out $DIR/GCTA/$TRAIT{1}" ::: $(seq 1 7)
 
 # BLUP solutions for the SNP effects
-plink1.9b6.10 --bfile data/data --keep tmp/$TEST --maf 0.01 --geno 0.05 --make-bed --out tmp/$TEST
+plink1.9b6.10 --bfile data/data --keep tmp/$BFILE --maf 0.01 --geno 0.05 --make-bed --out tmp/$BFILE
 software/gcta64 --bfile tmp/$BFILE --blup-snp $DIR/GCTA/${TRAIT}_${CV}_${MODEL}.indi.blp --autosome --out $DIR/GCTA/${TRAIT}_${CV}_${MODEL}
 
 # Then use plink --score $DIR/GCTA/height1.snp.blp 1 2 3 to do the prediction on the test set, ie bfile for the test set
