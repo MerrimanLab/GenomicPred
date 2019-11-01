d55a074ee9afa6c1628492d1ce5e4cd14f119083
Murray Cadzow <murray.cadzow@otago.ac.nz>
2019-11-01 15:28:37 +1300

    bayesR script with proper paths to program

--- scripts/run_bayesR.sh
+++ scripts/run_bayesR.sh
@@ -12,11 +12,13 @@ REGRESSION=$(grep -w "$TRAIT" data/pop_trait_models.csv | cut -d',' -f2)
 
 # run bayes
 # need 3 blank columns first as bayes counts from the 6th not the third
-mv tmp/${BFILE}.fam tmp/${BFILE}.fam.bak
-cp tmp/${BFILE}.pheno tmp/${BFILE}.fam
-software/bayesR -bfile tmp/${BFILE} -out $DIR/bayesR/${TRAIT}_${CV}_${MODEL} -numit 1000 -burnin 200 -n $PHENOCOL -seed 333
 
-software/bayesR -bfile tmp/${POP}_${TRAIT}_testing_${CV} -out $DIR/bayesR/${TRAIT}_${CV}_${MODEL}.2 -predict -model $DIR/bayesR/${TRAIT}_${CV}_${MODEL}.model -freq $DIR/bayesR/${TRAIT}_${CV}_${MODEL}.frq -param $DIR/bayesR/${TRAIT}_${CV}_${MODEL}.param
-mv tmp/${BFILE}.fam.bak tmp/${BFILE}.fam
+cp tmp/${BFILE}.pheno tmp/${BFILE}_${MODEL}.fam
+cp tmp/${BFILE}.bed tmp/${BFILE}_${MODEL}.bed
+cp tmp/${BFILE}.bim tmp/${BFILE}_${MODEL}.bim
+software/bayesR/bin/bayesR -bfile tmp/${BFILE}_${MODEL} -out $DIR/bayesR/${TRAIT}_${CV}_${MODEL} -numit 1000 -burnin 200 -n $PHENOCOL -seed 333
+
+software/bayesR/bin/bayesR -bfile tmp/${POP}_${TRAIT}_testing_${CV} -out $DIR/bayesR/${TRAIT}_${CV}_${MODEL}.2 -predict -model $DIR/bayesR/${TRAIT}_${CV}_${MODEL}.model -freq $DIR/bayesR/${TRAIT}_${CV}_${MODEL}.frq -param $DIR/bayesR/${TRAIT}_${CV}_${MODEL}.param
+#rm tmp/${BFILE}_${MODEL}.{fam,bed,bim}
 
 
