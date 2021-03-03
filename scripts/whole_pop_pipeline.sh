bf9ca9463f9edd85b5108dc8ef2bbef718034c93
Ben Rangihuna <Benrangihuna@gmail.com>
2021-03-04 11:07:55 +1300

    testing

--- scripts/whole_pop_pipeline.sh
+++ scripts/whole_pop_pipeline.sh
@@ -23,24 +23,16 @@ date
 
 
 #### Initial setup #####
-mkdir -p tmp ${RESULTS}/{GCTA,LDAK}
+mkdir -p tmp ${RESULTS}/
 
-software/plink1.9b6.10 --bfile ${ORIG_DATA} --make-bed --out tmp/data_sorted
+ software/plink1.9b6.10 --bfile ${ORIG_DATA} --make-bed --out tmp/data_sorted
 
 # create lise of independent SNPs from data
 
-software/plink1.9b6.10 --bfile tmp/data_sorted --indep-pairwise 50 5 0.2 --maf 0.1 --out tmp/data_pca_markers
+ software/plink1.9b6.10 --bfile tmp/data_sorted --indep-pairwise 50 5 0.2 --maf 0.1 --out tmp/data_pca_markers
+
 ## sort and keep our population
-
-
-mkdir -p tmp
-
 software/plink1.9b6.10 --bfile tmp/data_sorted --keep data/${POP}.keep --maf 0.01 --geno 0.05 --make-bed --out tmp/${POP}_sorteddata
-
-
-
-
-
 
 
 ######################## GCTA
@@ -48,19 +40,14 @@ software/plink1.9b6.10 --bfile tmp/data_sorted --keep data/${POP}.keep --maf 0.0
 #grm
 
 # only run this line once per population to save time
-
 software/gcta64 --bfile tmp/${POP}_sorteddata --autosome --make-grm --out ${RESULTS}/${POP}_gcta_grm --threads 4
-
-
-
 
 
 
 ######################## PCAs
 
 ### calculate principal components for the population using the independent markers
-
-software/plink1.9b6.10 --bfile tmp/${POP}_sorteddata --pca 10 --out ${RESULTS}/${POP}_pcafile --extract data/data_pca_markers.prune.in
+ software/plink1.9b6.10 --bfile tmp/${POP}_sorteddata --pca 10 --out ${RESULTS}/${POP}_pcafile --extract data/data_pca_markers.prune.in
 
 
 
@@ -68,29 +55,27 @@ software/plink1.9b6.10 --bfile tmp/${POP}_sorteddata --pca 10 --out ${RESULTS}/$
 ####################### LDAK
 
 ### calculate the weights and kinships for LDAK prior to GRM
-
-software/LDAK/ldak5.linux --cut-weights $DIR/${POP}_ldak_sections --bfile tmp/${POP}_sorteddata
-
+ software/LDAK/ldak5.linux --cut-weights ${RESULTS}/${POP}_ldak_sections --bfile tmp/${POP}_sorteddata
 
 
-
-software/LDAK/ldak5.linux --calc-weights-all $DIR/${POP}_ldak_sections --bfile tmp/${POP}_sorteddata
-
+ software/LDAK/ldak5.linux --calc-weights-all ${RESULTS}/${POP}_ldak_sections --bfile tmp/${POP}_sorteddata
 
 
-
-software/LDAK/ldak5.linux --calc-kins-direct $DIR/${POP}_ldak_kinships --bfile  tmp/${POP}_sorteddata --weights $DIR/${POP}_ldak_sections/weights.short --power -0.25
-
+software/LDAK/ldak5.linux --calc-kins-direct ${RESULTS}/${POP}_ldak_kinships --bfile  tmp/${POP}_sorteddata --weights ${RESULTS}/${POP}_ldak_sections/weights.short --power -0.25
 
 
+# example line to use for testing one iteration
+#line="HEIGHT,linear",
 while read line
 do
 	TRAIT=$(echo $line | cut -d',' -f1)
 	REGRESSION_TYPE=$(echo $line | cut -d',' -f2)
-
+	mkdir -p ${RESULTS}/{GCTA,LDAK}
 	##### Generate regression residuals for each model #####
 	# makes tmp/${POP}_${TRAIT}_residuals.txt
-	bash scripts/generate_models_whole_pop.sh ${POP} ${TRAIT} ${REGRESSION_TYPE} 
+	
+	#bash scripts/generate_models_whole_pop.sh ${POP} ${TRAIT} ${REGRESSION_TYPE} 
+	Rscript scripts/model_residuals_whole_pop.R --trait ${TRAIT} --pop ${POP} --regression ${REGRESSION_TYPE} --out_dir ${RESULTS}/${TRAIT}
 
 	##### Create phone files with the residuals
 	### prune and sort the data
@@ -101,12 +86,12 @@ do
 	# columns 8-12 are the FIVE residuals
 	# height, egfr, serumurate, diabetes and gout in that order
 	# need to join based on number fo models 2.2 - 2.8  was for 7 models
-	software/plink1.9b6.10 --bfile data/data --keep tmp/${POP}.keep --make-bed --out tmp/${POP}_${TRAIT}
+	software/plink1.9b6.10 --bfile data/data --keep data/${POP}.keep --make-bed --out tmp/${POP}_${TRAIT}
 	# this should join using IID from fam and IID from the pheno (in pheno first and second cols are duplicated) includes entire fam in output so need to adjust accordingly for model numbers (-6 cols)
-	join -1 2 -2 1 -o auto -e "NA" <(sort -k1 tmp/${POP}.fam) <(sort -k1 tmp/${POP}_${TRAIT}_residuals.txt | cut -f2- )  > tmp/${POP}_${TRAIT}.pheno 
+	join -1 2 -2 1 -o auto -e "NA" <(sort -k1 tmp/${POP}_${TRAIT}.fam) <(sort -k1 tmp/${POP}_${TRAIT}.residuals.txt | cut -f2- )  > tmp/${POP}_${TRAIT}.pheno 
 
 	# calculate the column indexes, offset by 2 (to account for FID and IID) from all of the residuals -> should be equal to $(seq 1 number_of_models)
-	pheno_cols=$( head -1 tmp/${POP}_${TRAIT}_residuals.txt | awk '{ for(i=1;i<=NF-2;i++){print i}}' | sort -u)	
+	pheno_cols=$( head -1 tmp/${POP}_${TRAIT}.residuals.txt | awk '{ for(i=1;i<=NF-2;i++){print i}}' | sort -u)	
 
 
 	#### GCTA REML for each model of current trait
