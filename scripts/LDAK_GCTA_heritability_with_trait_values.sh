f5981dab13b538403a7bc5ddee9edcca6c1e65de
Ben Rangihuna <Benrangihuna@gmail.com>
2021-07-14 11:18:29 +1200

    update prevalences

--- scripts/LDAK_GCTA_heritability_with_trait_values.sh
+++ scripts/LDAK_GCTA_heritability_with_trait_values.sh
@@ -1,10 +1,11 @@
-
+echo NPH
 POP=nphpca
 RESULTS=${POP}_results
+TRAIT=gout
 mkdir -p ${RESULTS}/{LDAK,GCTA}/${TRAIT}
 for covar in ls ${RESULTS}/*.covar
 do
- TRAIT=gout
+
  MODEL=$(basename ${covar} .covar)
  PREV=0.049
  mkdir -p ${RESULTS}/{LDAK,GCTA}/${TRAIT}
@@ -13,9 +14,9 @@ do
 
 done > ${POP}_${TRAIT}.log
 
+TRAIT=t2d
 for covar in ls ${RESULTS}/*.covar
 do
- TRAIT=t2d
  MODEL=$(basename ${covar} .covar)
  PREV=0.078
  mkdir -p ${RESULTS}/{LDAK,GCTA}/${TRAIT}
@@ -25,26 +26,26 @@ done > ${POP}_${TRAIT}.log
 
 
 ## East
-
+echo East
 POP=eastpca
 RESULTS=${POP}_results
+TRAIT=gout
 mkdir -p ${RESULTS}/{LDAK,GCTA}/${TRAIT}
 for covar in ls ${RESULTS}/*.covar
 do
- TRAIT=gout
  MODEL=$(basename ${covar} .covar)
- PREV=0.049
+ PREV=0.043
  mkdir -p ${RESULTS}/{LDAK,GCTA}/${TRAIT}
  software/LDAK/ldak5.linux --reml ${RESULTS}/LDAK/${TRAIT}/${MODEL} --pheno data/tanya_${TRAIT}.pheno  --grm ${RESULTS}/${POP}_ldak_kinships --prevalence ${PREV} --covar ${covar}
  software/gcta64 --reml --reml-pred-rand --grm ${RESULTS}/${POP}_gcta_grm --pheno data/tanya_${TRAIT}.pheno --prevalence ${PREV} --out ${RESULTS}/GCTA/${TRAIT}/${MODEL} --qcovar ${covar} --threads 4
 
 done > ${POP}_${TRAIT}.log
 
+TRAIT=t2d
 for covar in ls ${RESULTS}/*.covar
 do
- TRAIT=t2d
  MODEL=$(basename ${covar} .covar)
- PREV=0.078
+ PREV=0.084
  mkdir -p ${RESULTS}/{LDAK,GCTA}/${TRAIT}
  software/LDAK/ldak5.linux --reml ${RESULTS}/LDAK/${TRAIT}/${MODEL} --pheno data/tanya_${TRAIT}.pheno  --grm ${RESULTS}/${POP}_ldak_kinships --prevalence ${PREV} --covar ${covar}
   software/gcta64 --reml --reml-pred-rand --grm ${RESULTS}/${POP}_gcta_grm --pheno data/tanya_${TRAIT}.pheno --prevalence ${PREV} --out ${RESULTS}/GCTA/${TRAIT}/${MODEL} --qcovar ${covar} --threads 4
@@ -54,23 +55,24 @@ done > ${POP}_${TRAIT}.log
 
 POP=westpca
 RESULTS=${POP}_results
+TRAIT=gout
 mkdir -p ${RESULTS}/{LDAK,GCTA}/${TRAIT}
 for covar in ls ${RESULTS}/*.covar
 do
- TRAIT=gout
  MODEL=$(basename ${covar} .covar)
- PREV=0.049
+ PREV=0.051
  mkdir -p ${RESULTS}/{LDAK,GCTA}/${TRAIT}
  software/LDAK/ldak5.linux --reml ${RESULTS}/LDAK/${TRAIT}/${MODEL} --pheno data/tanya_${TRAIT}.pheno  --grm ${RESULTS}/${POP}_ldak_kinships --prevalence ${PREV} --covar ${covar}
  software/gcta64 --reml --reml-pred-rand --grm ${RESULTS}/${POP}_gcta_grm --pheno data/tanya_${TRAIT}.pheno --prevalence ${PREV} --out ${RESULTS}/GCTA/${TRAIT}/${MODEL} --qcovar ${covar} --threads 4
 
 done > ${POP}_${TRAIT}.log
 
+TRAIT=t2d
 for covar in ls ${RESULTS}/*.covar
 do
- TRAIT=t2d
+
  MODEL=$(basename ${covar} .covar)
- PREV=0.078
+ PREV=0.146
  mkdir -p ${RESULTS}/{LDAK,GCTA}/${TRAIT}
  software/LDAK/ldak5.linux --reml ${RESULTS}/LDAK/${TRAIT}/${MODEL} --pheno data/tanya_${TRAIT}.pheno  --grm ${RESULTS}/${POP}_ldak_kinships --prevalence ${PREV} --covar ${covar}
   software/gcta64 --reml --reml-pred-rand --grm ${RESULTS}/${POP}_gcta_grm --pheno data/tanya_${TRAIT}.pheno --prevalence ${PREV} --out ${RESULTS}/GCTA/${TRAIT}/${MODEL} --qcovar ${covar} --threads 4
@@ -82,23 +84,23 @@ done > ${POP}_${TRAIT}.log
 
 POP=europca
 RESULTS=${POP}_results
+TRAIT=gout
 mkdir -p ${RESULTS}/{LDAK,GCTA}/${TRAIT}
 for covar in ls ${RESULTS}/*.covar
 do
- TRAIT=gout
  MODEL=$(basename ${covar} .covar)
- PREV=0.049
+ PREV=0.024
  mkdir -p ${RESULTS}/{LDAK,GCTA}/${TRAIT}
  software/LDAK/ldak5.linux --reml ${RESULTS}/LDAK/${TRAIT}/${MODEL} --pheno data/tanya_${TRAIT}.pheno  --grm ${RESULTS}/${POP}_ldak_kinships --prevalence ${PREV} --covar ${covar}
  software/gcta64 --reml --reml-pred-rand --grm ${RESULTS}/${POP}_gcta_grm --pheno data/tanya_${TRAIT}.pheno --prevalence ${PREV} --out ${RESULTS}/GCTA/${TRAIT}/${MODEL} --qcovar ${covar} --threads 4
 
 done > ${POP}_${TRAIT}.log
 
+TRAIT=t2d
 for covar in ls ${RESULTS}/*.covar
 do
- TRAIT=t2d
  MODEL=$(basename ${covar} .covar)
- PREV=0.078
+ PREV=0.049
  mkdir -p ${RESULTS}/{LDAK,GCTA}/${TRAIT}
  software/LDAK/ldak5.linux --reml ${RESULTS}/LDAK/${TRAIT}/${MODEL} --pheno data/tanya_${TRAIT}.pheno  --grm ${RESULTS}/${POP}_ldak_kinships --prevalence ${PREV} --covar ${covar}
   software/gcta64 --reml --reml-pred-rand --grm ${RESULTS}/${POP}_gcta_grm --pheno data/tanya_${TRAIT}.pheno --prevalence ${PREV} --out ${RESULTS}/GCTA/${TRAIT}/${MODEL} --qcovar ${covar} --threads 4
